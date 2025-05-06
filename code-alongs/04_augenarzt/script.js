801da5701602a37f24aad0b4e057fb5ab269da15
Andrin Zuend <andrin.zuend@stud.fhgr.ch>
2025-05-06 09:12:54 +0200

    12
    12

--- code-alongs/04_augenarzt/script.js
+++ code-alongs/04_augenarzt/script.js
@@ -1,7 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const eye = document.getElementById("eye");
+const auge = document.querySelector("#eye");
+const pupille = document.querySelector("#pupil");
 
-    eye.addEventListener("click", () => {
-        eye.style.backgroundColor = "red";
-    });
-});
+auge.addEventListener('click', function(event) {
+    auge.classList.toggle('bloody');
+  });
+
+  setInterval(function() {
+    auge.classList.toggle('closed');
+  }, 2000)
+
+
+  window.addEventListener('mousemove', function (event) {
+    let mouse_x = event.clientX
+    if(mouse_x < window.innerWidth / 3){
+        pupille.classList.add('look_left');
+    } else if(mouse_x < window.innerWidth / 3 * 2) {
+        pupille.classList.remove('look_left');
+        pupille.classList.remove('look_right');
+    } else {
+        pupille.classList.add('look_right');
+    }
+     })
--- code-alongs/05_videoplayer/script.js
+++ code-alongs/05_videoplayer/script.js
@@ -1,1 +1,10 @@
-console.log('hi script.js')
+const video = document.querySelector('#video');
+const btnPlay = document.querySelector('#play');
+const btnPause = document.querySelector('#pause');
+const btnMute = document.querySelector('#mute');
+const btnUnmute = document.querySelector('#unmute');
+const time = document.querySelector('#time');
+
+pausebutton.classList.add('hidden');
+mutebutton.classList.add('hidden');
+
